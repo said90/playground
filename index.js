56d8de6fd25e3d34d7c1028b4712e3ac4d14b7ee
Said Franco <bryan.franco37@gmail.com>
2020-05-28 07:29:26 -0600

    Adding local interaction

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
-
+var contracts=[
+  {
+    Clause: "16.4",
+    No: "Sup.001",
+    Details:
+      "The annual pricing review cannot increase prices by more than then latest published inflation rate in USA1	",
+    Critically: "High",
+    Owner: "Max Jones, London",
+    task:[
+      {
+      checkFrecuency:'Annually, no later than 31 July',
+      details: 'Annual CPI Report',
+      provided: 'Finance Dept',
+      checkingMethod: 'Calculate % difference  between  previous  year pricing and  any new pricing, compare against latest CPI figures',
+      owner:"John smith"
+     }
+    ]
+  },
+  {
+    Clause: "29",
+    No: "Jnt.001",
+    Details:
+      "Each aprty must promptly advise the other if their address for service of notices changes	",
+    Critically: "Low",
+    Owner: "Ian black, New York	",
+    task:[
+      {
+      checkFrecuency:'Quarterly',
+      details: 'Quarterly software usage report',
+      provided: 'IT Dept, Sydney',
+      checkingMethod: 'Confirm receipt by cc of email to supplier advising quartely  license usage figures',
+      owner:"John smith"
+     }
+    ]
+  },
+  {
+    Clause: "8.9(b)",
+    No: "Org.001",
+    Details:
+      "The organization must submit a quarterly license usage report to the supplier within 10 bussines days of the end of each busssiness quarter	",
+    Critically: "Medium",
+    Owner: "Frank Saxon, Sydney",
+    task:[
+      {
+        checkFrecuency:'Bi annually, in Juanary  and July',
+        details: 'Curren head office address',
+        provided: 'Legal  Dept, New York',
+        checkingMethod: 'Standard email to suppliers advising the organization current address and requesting same from the supplier',
+        owner:"John smith"
+       }
+    ]
+  },
+];
+var source;
 $(document).ready(function () {
   var index;
   var indexSelected;
-  var data = [
-    {
-      Clause: "16.4",
-      No: "Sup.001",
-      Details:
-        "The annual pricing review cannot increase prices by more than then latest published inflation rate in USA1	",
-      Critically: "High",
-      Owner: "Max Jones, London",
-    },
-    {
-      Clause: "29",
-      No: "Jnt.001",
-      Details:
-        "Each aprty must promptly advise the other if their address for service of notices changes	",
-      Critically: "Low",
-      Owner: "Ian black, New York	",
-    },
-    {
-      Clause: "8.9(b)",
-      No: "Org.001",
-      Details:
-        "The organization must submit a quarterly license usage report to the supplier within 10 bussines days of the end of each busssiness quarter	",
-      Critically: "Medium",
-      Owner: "Frank Saxon, Sydney",
-    },
-  ];
+  var table2 = $("#table-task").DataTable({
+    data: source,
+    responsive: true,
+    columns: [
+      { data: "checkFrecuency" },
+      { data: "details" },
+      { data: "provided"},
+      { data: "checkingMethod" },
+      { data: "owner" }
+    ],           
+    bDestroy: true,     
+    paging: true,
+    ordering: true,
+  });  
 
-
-  var table = $("#table").DataTable({
-    data: data,
+var table = $("#table").DataTable({
+    data: contracts,
     responsive: true,
     columns: [
       { data: "Clause" },
@@ -48,6 +88,67 @@ $(document).ready(function () {
     paging: true,
     ordering: true,
   });  
+
+  $('#modalForm').draggable({
+    handle:".card-header"
+  });
+
+  $('#modal-task').draggable({
+    handle:".card-header"
+  });
+
+  $('#add-button').on('click',function(){
+    $('#confirm-button').html( "Create");
+    $('.card-title').html('Adding New Clause');
+    index='';
+  });
+
+  $("#table").on("click", ".btn-warning", function (e) {
+    var $this = $(this);
+    index= this.parentNode.parentNode.rowIndex;
+    var row = this.parentNode.parentNode;
+    $('.card-title').html('Modifying Clause');
+    $('#confirm-button').html( "Modify");
+    $("#clause-number").val(row.cells[0].innerHTML);
+    $("#clause").val(row.cells[1].innerHTML);
+    $("#clause-detail").val(row.cells[2].innerHTML);
+    $("#critical-level").val(row.cells[3].innerHTML);
+    $("#clause-owner").val(row.cells[4].innerHTML);
+  });
+
+  $('#modalForm').on('hidden.bs.modal', function (e) {
+    $("#clause-number").val('');
+    $("#clause").val('');
+    $("#clause-detail").val('');
+    $("#critical-level").val('');
+    $("#clause-owner").val('');
+  });
+
+  $('#modal-task').on('hidden.bs.modal', function (e) {
+    $('#check-frecuency').val('');
+    $('#task-detail').val('');
+    $('#provided-task').val('');
+    $('#task-checking-method').val('');
+    $('#clause-owner-task').val('');
+
+  });
+
+  
+  $("#table tbody").on("click", "tr", function () {
+    console.log(this.rowIndex);
+    if ($(this).hasClass("selected")) {
+      index=0;
+      $(this).removeClass("selected");
+      $('#btn-task').attr("disabled",true);
+     
+    } else {
+      table.$("tr.selected").removeClass("selected");
+      $(this).addClass("selected");
+      index=this.rowIndex;
+      $('#btn-task').attr("disabled",false);
+      
+    }
+  });
     $(document).on('click', '.modal-confirm', function (e) {
 		e.preventDefault();
     $('#modalForm').modal('hide')
@@ -60,11 +161,12 @@ $(document).ready(function () {
 		var t = $('#table').DataTable();
 
 		var clauseData= {
-	     "Clause":       clause,
-        "No":   clauseNumber,
-        "Details":    clauseDetail,
-        "Critically": criticalLevel,
-        "Owner":     clauseOwner
+	      Clause:       clause,
+        No:   clauseNumber,
+        Details:    clauseDetail,
+        Critically: criticalLevel,
+        Owner:     clauseOwner,
+        task:[]
     }
   
   $('#clause-number').val('');
@@ -72,7 +174,13 @@ $(document).ready(function () {
   $('#clause-detail').val('');
   $('#critical-level').val('');
   $('#clause-owner').val('');
+  console.log(index);
     if (index) {
+      // contracts[index-1].task = typeof contracts[index-1].task !=undefined ? contracts[index-1].task :[];
+      // contracts[index-1] = clauseData;
+      // table.clear();
+      // table.rows.add(contracts);
+      // table.draw();
       var rows = document.getElementById("table").rows;
       rows[index].cells[0].innerHTML =clauseNumber;
       rows[index].cells[1].innerHTML=clause;
@@ -89,7 +197,7 @@ $(document).ready(function () {
 		td.innerHTML = '<i data-toggle class="far fa-plus-square text-primary h5 m-0" style="cursor: pointer;"></i>';
 		td.className = "text-center";
 		var $table = $('#datatable-details');
-		data.push(clauseData);
+		contracts.push(clauseData);
 
 		$('#datatable-details > tbody:last-child').append('<tr>'+'<td>'+ clauseNumber+'</td>'+ '<td>'+ clause+'</td>'+'<td>'+ clauseDetail+'</td>'+'<td>'+ criticalLevel+'</td>'+'<td>'+ clauseOwner+'</td>'+' <td class="actions"> <a ><i class="fas fa-pencil-alt"></i></a> <a  class="delete-row"><i class="far fa-trash-alt"></i></a>   </td>'+'</tr>');
 		// insert the expand/collapse column
@@ -127,119 +235,51 @@ $(document).ready(function () {
 
   });
   
-$('#modalForm').draggable({
-  handle:".card-header"
-});
-
-$('#modal-task').draggable({
-  handle:".card-header"
-});
-
-$('#add-button').on('click',function(){
-  $('#confirm-button').html( "Create");
-  $('.card-title').html('Adding New Clause');
-  index='';
-});
-
-  $("#table").on("click", ".btn-warning", function (e) {
-    var $this = $(this);
-    index= this.parentNode.parentNode.rowIndex;
-    var row = this.parentNode.parentNode;
-    $('.card-title').html('Modifying Clause');
-    $('#confirm-button').html( "Modify");
-    $("#clause-number").val(row.cells[0].innerHTML);
-    $("#clause").val(row.cells[1].innerHTML);
-    $("#clause-detail").val(row.cells[2].innerHTML);
-    $("#critical-level").val(row.cells[3].innerHTML);
-    $("#clause-owner").val(row.cells[4].innerHTML);
+  $('#confirm-button-task').on("click", function () {
+    console.log($('#clause-owner-task').val());
+    var task = {
+      checkFrecuency:$('#check-frecuency').val(),
+      details: $('#task-detail').val(),
+      provided: $('#provided-task').val(),
+      checkingMethod: $('#task-checking-method').val(),
+      owner: $('#clause-owner-task').val()
+    };
+    contracts[index-1].task.push(task);
+    table2.clear();
+    table2.rows.add(contracts[index-1].task);
+    table2.draw();
+      $('#check-frecuency').val('');
+      $('#task-detail').val('');
+      $('#provided-task').val('');
+      $('#task-checking-method').val('');
+      $('#clause-owner-task').val('');
+      $('#modal-task').modal('hide');
   });
 
-  
-
-  $('#modalForm').on('hidden.bs.modal', function (e) {
-    $("#clause-number").val('');
-    $("#clause").val('');
-    $("#clause-detail").val('');
-    $("#critical-level").val('');
-    $("#clause-owner").val('');
-  })
-
-  $("#table tbody").on("click", "tr", function () {
-    if ($(this).hasClass("selected")) {
-      $(this).removeClass("selected");
-    } else {
-      table.$("tr.selected").removeClass("selected");
-      $(this).addClass("selected");
-    }
-  });
-    
 $('#table').on("click","tr", function(e){
-    console.log(this.rowIndex);
-    var task1= 
-    [
-      {
-        checkFrecuency:'Annually, no later than 31 July',
-      details: 'Annual CPI Report',
-      provided: 'Finance Dept',
-      checkingMethod: 'Calculate % difference  between  previous  year pricing and  any new pricing, compare against latest CPI figures'
-     }
-    ];
-
-    var task2= 
-    [
-      {
-        checkFrecuency:'Quarterly',
-      details: 'Quarterly software usage report',
-      provided: 'IT Dept, Sydney',
-      checkingMethod: 'Confirm receipt by cc of email to supplier advising quartely  license usage figures'
-     }
-    ];
-
-    var task3= 
-    [
-      {
-      checkFrecuency:'Bi annually, in Juanary  and July',
-      details: 'Curren head office address',
-      provided: 'Legal  Dept, New York',
-      checkingMethod: 'Standard email to suppliers advising the organization current address and requesting same from the supplier'
-     }
-    ];
-
-    var table2 = $("#table-task").DataTable({
-      data: source,
-      responsive: true,
-      columns: [
-        { data: "checkFrecuency" },
-        { data: "details" },
-        { data: "provided"},
-        { data: "checkingMethod" },
-        
-      ],           
-      bDestroy: true,     
-      paging: true,
-      ordering: true,
-    });  
-    var source;
-    if (this.rowIndex == 1) {
-        table2.clear();
-        table2.rows.add(task1);
-       table2.draw();
-    };
-    if (this.rowIndex == 2) {
-
+    if (typeof contracts[this.rowIndex-1].task != undefined) {
       table2.clear();
-        table2.rows.add(task2);
-       table2.draw();
-    };
-
-    if (this.rowIndex == 3) {
-      table2.clear();
-      table2.rows.add(task3);
+      table2.rows.add(contracts[this.rowIndex-1].task);
       table2.draw();
+    }else{
+      table2.clear();
+      table2.draw();
+    }
+        
+    // if (this.rowIndex == 2) {
 
-    };
+    //   table2.clear();
+    //     table2.rows.add(contracts[1].task);
+    //    table2.draw();
+    // };
+
+    // if (this.rowIndex == 3) {
+    //   table2.clear();
+    //   table2.rows.add(contracts[2].task);
+    //   table2.draw();
+    // };
     
 
 }); 
-  
+ 
 });
